6120d9824c2a73ab674d894d9c6267f1ef44b769
Time6628 <pturchan@yahoo.com>
2016-03-26 13:15:04 -0400

    Version 1.1.1: - Fixed stuff

--- src/main/java/com/kookykraftmc/witcherycmd/ModInfo.java
+++ src/main/java/com/kookykraftmc/witcherycmd/ModInfo.java
@@ -7,7 +7,7 @@ public class ModInfo {
     public static final String ID = "witcherycmd";
     public static final String CHANNEL = "WCmd";
     public static final String DEPEND = "Witchery";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.1";
     public static final String CLIENTPROXY = "com.kookykraftmc.witcherycmd.proxies.ClientProxy";
     public static final String COMMONPROXY = "com.kookykraftmc.witcherycmd.CommonProxy";
 }
